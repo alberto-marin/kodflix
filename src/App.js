b809b0b412420a5ec125c07d91978074e5b6d9d4
alberto <albertomr84@gmail.com>
2019-10-26 19:59:54 +0100

    add 6 titles

--- src/App.js
+++ src/App.js
@@ -9,6 +9,30 @@ function App() {
         <img src={javascriptLogo} className="App-logo" alt="logo" />
         <h1>Welcome to Kodflix</h1>
       </header>
+      <main>
+        <section className="three-columns">
+          <div className="column">
+            <h2>ReactJs</h2>
+          </div>
+          <div className="column">
+            <h2>HTML5</h2>
+          </div>
+          <div className="column">
+            <h2>CSS3</h2>
+          </div>
+        </section>
+        <section className="three-columns">
+          <div className="column">
+            <h2>Node.js</h2>
+          </div>
+          <div className="column">
+            <h2>Mongodb</h2>
+          </div>
+          <div className="column">
+            <h2>Heroku</h2>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
