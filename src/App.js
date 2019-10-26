3845004e9357019f0292d9c889264dcd845c4a2b
alberto <albertomr84@gmail.com>
2019-10-26 21:12:22 +0100

    add images and style them

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import javascriptLogo from './javascriptLogo.svg';
+import javascriptLogo from './assets/img/javascriptLogo.svg';
+import css3Image from './assets/img/css-3.svg';
+import databaseImage from './assets/img/database.svg';
+import herokuImage from './assets/img/heroku.svg';
+import html5Image from './assets/img/html5.svg';
+import nodejsImage from './assets/img/nodejs.svg';
+import reactImage from './assets/img/react.svg';
 import './App.css';
 
 function App() {
@@ -10,29 +16,32 @@ function App() {
         <h1>Welcome to Kodflix</h1>
       </header>
       <main>
-        <section className="three-columns">
+        <div className="three-columns">
           <div className="column">
-            <h2>ReactJs</h2>
+            <img src={reactImage} alt="react logo" className="image-logo" />
           </div>
           <div className="column">
-            <h2>HTML5</h2>
+            <img src={html5Image} alt="html5 logo" className="image-logo" />
           </div>
           <div className="column">
-            <h2>CSS3</h2>
+            <img src={css3Image} alt="css3 logo" className="image-logo" />
           </div>
-        </section>
-        <section className="three-columns">
+        </div>
+        <div className="three-columns">
           <div className="column">
-            <h2>Node.js</h2>
-          </div>
-          <div className="column">
-            <h2>MongoDB</h2>
+            <img src={nodejsImage} alt="nodejs logo" className="image-logo" />
           </div>
           <div className="column">
-            <h2>Heroku</h2>
+            <img src={databaseImage} alt="database logo" className="image-logo" />
           </div>
-        </section>
+          <div className="column">
+            <img src={herokuImage} alt="heroku logo" className="image-logo" />
+          </div>
+        </div>
       </main>
+      <footer>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      </footer>
     </div>
   );
 }
