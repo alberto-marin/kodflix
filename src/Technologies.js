1c63596c6f6f78e4fb2466eb2a5e9960f8cfca7e
alberto <albertomr84@gmail.com>
2019-10-27 08:02:16 +0000

    create data source

--- src/Technologies.js
+++ src/Technologies.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import Column from './Column';
-import css3Image from './assets/img/css-3.svg';
-import databaseImage from './assets/img/database.svg';
-import herokuImage from './assets/img/heroku.svg';
-import html5Image from './assets/img/html5.svg';
-import nodejsImage from './assets/img/nodejs.svg';
-import reactImage from './assets/img/react.svg';
-
+import getTechnologies from './data-source/Technologies-get';
 
 function Technologies() {
   return (
     <div className="technologies">
       <section className="three-columns">
-        <Column id="react" name="ReactJs" logo={reactImage} />
-        <Column id="html5" name="HTML5" logo={html5Image} />
-        <Column id="css3" name="CSS3" logo={css3Image} />
-        <Column id="node" name="Node.js" logo={nodejsImage} />
-        <Column id="mongo" name="MongoDB" logo={databaseImage} />
-        <Column id="heroku" name="Heroku" logo={herokuImage} />
+        {
+          getTechnologies().map(technology => (
+            <Column
+              key={technology.id}
+              id={technology.id}
+              name={technology.name}
+              logo={technology.logo}
+            />
+          ))
+        }
       </section>
     </div>
   )
