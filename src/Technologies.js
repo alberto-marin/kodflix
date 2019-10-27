703e6bc1687cea5fa948c5b1ef4cb83176cfdae1
alberto <albertomr84@gmail.com>
2019-10-27 04:04:32 +0000

    responsive layout

--- src/Technologies.js
+++ src/Technologies.js
@@ -8,21 +8,19 @@ import nodejsImage from './assets/img/nodejs.svg';
 import reactImage from './assets/img/react.svg';
 
 
-function Technologies(){
-    return (
-        <div className="technologies">
-            <section className="three-columns">
-          <Column id="react" name="ReactJs" logo={reactImage} />
-          <Column id="html5" name="HTML5" logo={html5Image} />
-          <Column id="css3" name="CSS3" logo={css3Image} />
-        </section>
-        <section className="three-columns">
-          <Column id="node" name="Node.js" logo={nodejsImage} />
-          <Column id="mongo" name="MongoDB" logo={databaseImage} />
-          <Column id="heroku" name="Heroku" logo={herokuImage} />
-        </section>
-        </div>
-    )
+function Technologies() {
+  return (
+    <div className="technologies">
+      <section className="three-columns">
+        <Column id="react" name="ReactJs" logo={reactImage} />
+        <Column id="html5" name="HTML5" logo={html5Image} />
+        <Column id="css3" name="CSS3" logo={css3Image} />
+        <Column id="node" name="Node.js" logo={nodejsImage} />
+        <Column id="mongo" name="MongoDB" logo={databaseImage} />
+        <Column id="heroku" name="Heroku" logo={herokuImage} />
+      </section>
+    </div>
+  )
 }
 
 export default Technologies;
